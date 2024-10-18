7117a8feedabeacad8445951b9dfa8e2e8165af8
Halil <halilski1979@gmail.com>
2024-10-18 10:40:40 +0300

    ddw

---
+++ Loops/p08_Vlojeni_Cikli/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace p08_Vlojeni_Cikli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"i={i}");
+                for(int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine($"j={j}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Loops/p09_MagicNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace p09_MagicNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num=int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < 10; i++)
+            { 
+               for (int j = 1; j < 10; j++)
+                {
+                    for(int k = 1; k < 10; k++)
+                    {
+                        for (int l = 1; l < 10; l++)
+                        {
+                            for (int m = 1; m < 10; m++)
+                            {
+                                for (int n = 1; n < 10; n++)
+                                {
+                                    if (num==(i*j*k*l*m*n))
+                                    {
+                                        Console.WriteLine($"{i}{j}{k}{l}{m}{n}");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
