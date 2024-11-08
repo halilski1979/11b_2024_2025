290c20a995605a5b6a13ebfc641d879bd03ecbea
Halil <halilski1979@gmail.com>
2024-11-08 09:53:23 +0200

    dwadw

---
+++ Methods/P06 - Print/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace P06___Print
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Print();
+            Console.WriteLine();
+
+            int number = int.Parse(Console.ReadLine());
+            Pechat(number);
+        }
+
+        private static void Print()
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void Pechat(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
