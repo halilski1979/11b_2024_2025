4a462614f1a5f9b526d1ea245b376926fb7ef2ee
Halil <halilski1979@gmail.com>
2024-11-11 09:29:55 +0200

    xsax

---
+++ OOP/Class/DemoClass/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoClass
+{
+    public class Person
+    {
+        // 1. public - Модификатор,  2. string fName -  Полета на класа
+        public string fName;
+        public string sName;
+        public int age;
+
+        string adress;
+        string phoneNumber;
+
+    }
+}
---
+++ OOP/Class/DemoClass/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DemoClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.fName = "Ivan";
+            p1.sName = "Ivanov";
+            p1.age = 22;
+
+            Person p2 = new Person();
+            p2.fName = "Petar";
+            p2.sName = "Petrov";
+            p2.age = 23;
+
+            Console.WriteLine($"Name: {p1.fName} {p1.sName}, {p1.age} years old!");
+            Console.WriteLine($"Name: {p2.fName} {p2.sName}, {p2.age} years old!");
+
+        }
+    }
+}
