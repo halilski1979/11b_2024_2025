54b578da1dda9c2870bdbdec6c6828856df1c05a
Halil <halilski@abv.bg>
2024-11-12 12:51:30 +0200

    daww

---
+++ OOP/02. Methods/Methods_In_Class/p01_PublicMethods_DEMO/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace p01_PublicMethods_DEMO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Ivan", "Ivanov", 20);
+
+            // Извличане на пълното име
+            string fullName = person.GetFullName();
+            Console.WriteLine(fullName); // Извежда: Ivan Ivanov
+
+            // Проверка дали е пълнолетен
+            bool isAdult = person.IsAdult();
+            Console.WriteLine(isAdult); // Извежда: True
+
+            // Увеличаване на възрастта
+            person.IncreaseAge();
+            Console.WriteLine(person.Age); // Извежда: 21
+
+        }
+    }
+}
