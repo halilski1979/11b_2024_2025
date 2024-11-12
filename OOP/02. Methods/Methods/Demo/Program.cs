7864686d748d0cf04ea1e245655eee04c07c2fea
Halil <halilski1979@gmail.com>
2024-11-12 12:19:25 +0200

    ccasc

---
+++ OOP/02. Methods/Methods/Demo/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("Ivan", "Ivanov", 44);
+            Console.WriteLine(p.GetFullName()); 
+        }
+    }
+}
