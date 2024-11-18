a0b86dd4e2348189e325f3ab7f33e904b701a505
Halil <halilski1979@gmail.com>
2024-11-18 09:26:51 +0200

    dawd

---
+++ OOP/00. Construktors/Construktors/p00 - DemoConstructor/Person.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p00___DemoConstructor
+{
+    internal class Person
+    {
+		private string fName;
+        private string sName;
+        private int age;
+        private double salary;
+
+        public Person(string fName, string sName, int age, double salary)
+        {
+            this.FName = fName;
+            this.SName = sName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+		public Person()
+		{
+
+		}
+
+		public Person(string fName,string sName)
+		{
+			this.FName = fName;
+			this.SName = sName;
+		}
+
+		public Person(string ime,string familia, int godini)
+		{
+			this.FName= ime;
+			this.SName= familia;
+			this.Age = godini;
+		}
+
+        public string FName
+		{
+			get { return fName; }
+			set { fName = value; }
+		}		
+		public string SName
+		{
+			get { return sName; }
+			set { sName = value; }
+		}		
+		public int Age
+		{
+			get { return age; }
+			set { age = value; }
+		}		
+		public double Salary
+		{
+			get { return salary; }
+			set { salary = value; }
+		}
+
+
+		//Пренаписваме метода ToString()
+        public override string ToString()
+        {
+            return $"My name is {FName} {SName}. I am {Age} years odl." +
+				$"My salary is {Salary} lv. ";	
+        }
+
+
+
+
+    }
+}
---
+++ OOP/00. Construktors/Construktors/p00 - DemoConstructor/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace p00___DemoConstructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Person p = new Person("Halil","Dospatliyki",25,3000);
+            Person p1 = new Person();
+            Person p2 = new Person("Ivan","Ivanov");
+            Person p3 = new Person("Leo","Messi",36);
+
+            Console.WriteLine(p);
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
+           
+        }
+    }
+}
