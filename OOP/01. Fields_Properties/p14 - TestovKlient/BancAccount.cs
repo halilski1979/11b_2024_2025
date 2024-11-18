86a7902a28bf09e71f356c2ef72e2c7c55f0fa58
Halil <halilski1979@gmail.com>
2024-11-18 11:12:36 +0200

    фрвеф

---
+++ OOP/01. Fields_Properties/p14 - TestovKlient/BancAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p14___TestovKlient
+{
+    internal class BancAccount
+    {
+		private int id;
+        private double balance;
+
+        public int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}	
+
+		public double Balance
+		{
+			get { return balance; }
+			set { balance = value; }
+		}
+
+        public void Deposit(double amount)
+        {
+            this.balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            this.balance -= amount;
+        }
+
+        public override string ToString()
+        {
+            return $"Account ID{this.id}, balance {this.balance:f2}";
+        }
+
+    }
+}
---
+++ OOP/01. Fields_Properties/p14 - TestovKlient/Client.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p14___TestovKlient
+{
+    internal class Client
+    {
+        Dictionary<int, BancAccount> bankoviSmetki = new Dictionary<int, BancAccount>();
+
+        public void Create(int id)
+        {
+            if (bankoviSmetki.ContainsKey(id))
+            {
+                Console.WriteLine("Accounts already exist!");
+            }
+            else
+            {
+                var smetka = new BancAccount();
+                smetka.Id = id;
+                smetka.Balance = 1000;
+                //accaount.Balance = 1000;
+                bankoviSmetki.Add(id, smetka);
+
+            }
+        }
+        public void Deposit(int id, double amount)
+        {
+            if (!bankoviSmetki.ContainsKey(id))
+            {
+                Console.WriteLine("Account does not exist");
+            }
+            else 
+            {
+              var smetka =bankoviSmetki[id];
+              //BancAccount smetka = bankoviSmetki[id];
+
+                smetka.Deposit(amount);
+            }
+        }
+        public void Withdraw(int id, double amount)
+        {
+            BancAccount smetka = bankoviSmetki[id];
+
+            if (!bankoviSmetki.ContainsKey(id))
+            {
+                Console.WriteLine("Account does not exist");
+            }
+            
+            else
+            {
+                if (smetka.Balance < amount)
+                {
+                    Console.WriteLine("Insufficient balance");
+                   
+                }
+                else
+                {
+                    smetka.Withdraw(amount);
+                }
+            }           
+
+        }
+        public void Print(int id)
+        {
+            if (!bankoviSmetki.ContainsKey(id))
+            {
+                Console.WriteLine("Account does not exist");
+            }
+            else
+            {
+                BancAccount smetka = bankoviSmetki[id];
+                Console.WriteLine(smetka.ToString());
+            }
+        }
+
+
+
+    } 
+
+}
+
