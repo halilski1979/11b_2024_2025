2cbb7a47828b4bb45845c4196aea78113c09c56b
Halil <halilski1979@gmail.com>
2024-11-29 12:36:39 +0200

    dadaw

---
+++ OOP/03. Validation/P12 - PersonValidation_Construktor/Person.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P12___PersonValidation_Construktor
+{
+    internal class Person
+    {
+        public string FName { get; set; }
+        public string SName { get; set; }
+        public int Age { get; set; }
+        public double Salary { get; set; }
+
+        public static int counter;
+        
+       
+
+       
+
+        public Person(string fName, string sName, int age, double salary)
+        {
+            if (fName.Length<3)
+            {
+                throw new ArgumentException("First Name cannot be less than 3 symbols");
+            }
+
+            if (sName.Length < 3)
+            {
+                throw new ArgumentException("Second Name cannot be less than 3 symbols");
+            }
+
+            if (age < 0)
+            {
+                throw new ArgumentException("Age cannot be negative!");
+            }
+            if (salary < 500)
+            {
+                throw new ArgumentException("Salary cannot be less than 500 lv.");
+            }
+            FName = fName;
+            SName = sName;
+            Age = age;
+            Salary = salary;
+            counter++;
+        }
+
+
+       
+
+        public void IncreaseSalary(double bonus)
+        {
+            if (this.Age > 30)
+            {
+                this.Salary += this.Salary * bonus / 100;
+            }
+            else
+            {
+                this.Salary += this.Salary * bonus / 200;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{FName} {SName}, {Age} years old, {Salary} lv.";
+        }
+
+
+    }
+}
---
+++ OOP/03. Validation/P12 - PersonValidation_Construktor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace P12___PersonValidation_Construktor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                var person = new Person
+                (
+                    cmdArgs[0],
+                    cmdArgs[1],
+                    int.Parse(cmdArgs[2]),
+                    double.Parse(cmdArgs[3])
+                );
+                persons.Add(person);
+            }
+
+            foreach (var item in persons)
+            {
+                item.IncreaseSalary(20);
+                Console.WriteLine(item.ToString());
+            }
+
+            Console.WriteLine(Person.counter);
+        }
+    }
+}
